0a6525ca20f7f6c7814387497e16f8bf71d18dd6
Wyatt Preul <wpreul@gmail.com>
2012-12-10 11:17:44 -0600

    Adding tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 // Load modules
 
 var Chai = require('chai');
+var Joi = require('joi');
 var Lout = process.env.TEST_COV ? require('../lib-cov') : require('../lib');
 
 
@@ -18,6 +19,9 @@ describe('Lout', function () {
 
     var indexTemplate = '<html>index{{#each routes}}{{this.method}}{{/each}}</html>';
     var routeTemplate = '<html>route{{#each routes}}{{this.method}}{{/each}}</html>';
+    var routeTemplateWithQuery = '<html>{{#each routes}}{{#each this.queryParams}}{{this.required}}{{/each}}{{/each}}</html>';
+    var routeTemplateWithAllowed = '<html>{{#each routes}}{{#each this.queryParams}}{{#each this.allowedValues}}{{this}}{{/each}}{{/each}}{{/each}}</html>';
+    var routeTemplateWithDenied = '<html>{{#each routes}}{{#each this.queryParams}}{{#each this.disallowedValues}}{{this}}{{/each}}{{/each}}{{/each}}</html>';
 
     describe('#constructor', function () {
 
@@ -133,6 +137,118 @@ describe('Lout', function () {
 
             expect(markup).to.equal('<html>routeGET</html>');
             done();
+        });
+
+        it('handles a null route safely', function (done) {
+
+            var lout = new Lout({
+                indexTemplate: indexTemplate,
+                routeTemplate: routeTemplate
+            });
+
+            var markup = lout.generateRoutesMarkup([null]);
+
+            expect(markup).to.equal('<html>route</html>');
+            done();
+        });
+
+        describe('getting params data', function () {
+
+            it('processes a non-object validation property', function (done) {
+
+                var lout = new Lout({
+                    indexTemplate: indexTemplate,
+                    routeTemplate: routeTemplate
+                });
+
+                var markup = lout.generateRoutesMarkup([{
+                    method: 'POST',
+                    config: { validate: { query: 'test' } }
+                }]);
+
+                expect(markup).to.equal('<html>routePOST</html>');
+                done();
+            });
+
+            it('processes an empty object validation property', function (done) {
+
+                var lout = new Lout({
+                    indexTemplate: indexTemplate,
+                    routeTemplate: routeTemplate
+                });
+
+                var markup = lout.generateRoutesMarkup([{
+                    method: 'POST',
+                    config: { validate: { query: {} } }
+                }]);
+
+                expect(markup).to.equal('<html>routePOST</html>');
+                done();
+            });
+
+            it('processes a query validation object with required', function (done) {
+
+                var lout = new Lout({
+                    indexTemplate: indexTemplate,
+                    routeTemplate: routeTemplateWithQuery
+                });
+
+                var markup = lout.generateRoutesMarkup([{
+                    method: 'POST',
+                    config: { validate: { query: { username: Joi.Types.String().required() } } }
+                }]);
+
+                expect(markup).to.equal('<html>true</html>');
+                done();
+            });
+
+            it('processes a query validation object with allowed values', function (done) {
+
+                var lout = new Lout({
+                    indexTemplate: indexTemplate,
+                    routeTemplate: routeTemplateWithAllowed
+                });
+
+                var markup = lout.generateRoutesMarkup([{
+                    method: 'POST',
+                    config: { validate: { query: { username: Joi.Types.String().allow('hello').allow('hi') } } }
+                }]);
+
+                expect(markup).to.equal('<html>hellohi</html>');
+                done();
+            });
+
+            it('processes a query validation object with denied values', function (done) {
+
+                var lout = new Lout({
+                    indexTemplate: indexTemplate,
+                    routeTemplate: routeTemplateWithDenied
+                });
+
+                var markup = lout.generateRoutesMarkup([{
+                    method: 'POST',
+                    config: { validate: { query: { username: Joi.Types.String().deny('test') } } }
+                }]);
+
+                expect(markup).to.equal('<html>test</html>');
+                done();
+            });
+
+            it('processes a query validation object that has with', function (done) {
+
+                var lout = new Lout({
+                    indexTemplate: indexTemplate,
+                    routeTemplate: routeTemplateWithDenied
+                });
+
+                var markup = lout.generateRoutesMarkup([{
+                    method: 'POST',
+                    config: { validate: { query: { username: Joi.Types.String().with('test') } } }
+                }]);
+
+                expect(markup).to.equal('<html></html>');
+                done();
+            });
         });
     });
 });
