e3b23c9c0b9d37baebb44d476b5ca8bfc8a925a9
kpdecker <kpdecker@gmail.com>
2015-10-02 23:12:48 -0500

    Rework display to resemble JSON
    Mimic JSON structures with {key: type} and [type] structures. Extended validation data is pushed into collapsible sections to simplify the UI structure.
    
    Additionally reduces the vertical height of the UI where possible to help with UI cohesion.

---
+++ templates/helpers/collapse.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var Handlebars = require('handlebars');
+
+var uniqueId = 0;
+
+module.exports = function (options) {
+
+    var data = Handlebars.createFrame(options.data);
+    data.collapseId = uniqueId++;
+
+    var children = options.fn.partials.children(this).trim(),
+        content = options.fn(this).trim();
+    if (content) {
+        data.static = false;
+
+        var header = options.fn.partials.header(this, { data: data }).trim();
+        if (/data-toggle="collapse"/.test(header)) {
+            content = header
+                + '<div class="collapse" id="type' + data.collapseId + '">'
+                    + '<dl class="well">'
+                        + content
+                    + '</dl>'
+                + '</div>';
+        } else {
+            content = header
+                + '<div class="static-type">'
+                    + '<dl class="well">'
+                        + content
+                    + '</dl>'
+                + '</div>';
+        }
+    } else {
+        data.static = true;
+        content = options.fn.partials.header(this, { data: data });
+    }
+
+    return new Handlebars.SafeString(content + children);
+};
